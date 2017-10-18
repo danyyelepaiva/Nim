e9097fa91b72630006e3e8212a1a45fa3214327d
danyyelepaiva <up201305675@fc.up.pt>
2017-10-18 14:28:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function hide(eid){
+	document.getElementById(eid).style.display = 'none';
+}
+
+function show(eid){
+	document.getElementById(eid).style.display = 'inline-block';
+}
+
+function hide_login() {
+	//hide("top");
+	//if (esconde) hide("jogo");
+	//else show("jogo");
+	//show("juntar");
+	hide("login");
+	// if(modojogo==1) hide("bsair");
+	// return false;
+}
