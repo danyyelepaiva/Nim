e71d230e3bbc95d3f36ece4835f2abd5b34eb983
danyyelepaiva <danyyel_paiva@hotmail.com>
2017-10-27 21:41:43 +0100

    sxt 0.3

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function hide_login() {
 }
 
 function gerente(){
-    
+
 }
 
 function jogada(){
@@ -39,16 +39,6 @@ function jogada(){
   var linha;
   switch(tabSize){
     case 1:
-      tabCol=3;
-      tabLin = [1,2.3];
-      break;
-    case 2:
-      tabCol=3;
-      tabLin=[1,3,5];
-      break;
-    case 3:
-      tabCol=5;
-      tabLin=[1,2.3,4,5];
       break;
   }
   coluna = Math.floor((Math.random()*tabCol)+1);
@@ -108,11 +98,11 @@ function tableMaker(tam){
       break;
 
     case 2:
-      for(i=1; i<=3; i++){
+      for(i=1; i<=4; i++){
         coluna = document.createElement("div");
         coluna.setAttribute("class", "coluna");
         tabela.appendChild(coluna);
-        for(j=1; j<=(i*2)-1; j++){
+        for(j=1; j<=i; j++){
           peca = document.createElement("button");
           peca.setAttribute("id","peca"+i+j);/**/
           peca.setAttribute("onclick","peca("+i+","+j+");");/**/
